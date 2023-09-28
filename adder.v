cedb4ea8c62bd2c25cb59013f43410105e702a1b
CurieLuo <CurieLuo@qq.com>
2023-09-28 16:12:55 +0800

    initial commit

---
+++ adder.v
@@ -0,0 +1,17 @@
+/*
+    Problem:
+    https://acm.sjtu.edu.cn/OnlineJudge/problem?problem_id=1250
+ 
+    任务：掌握组合逻辑，完成一个超前进位加法器。
+*/
+
+
+
+module Add(
+        input       [31:0]          a,
+        input       [31:0]          b,
+        output reg  [31:0]          sum
+    );
+
+
+endmodule
